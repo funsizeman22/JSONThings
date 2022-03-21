453bf18ada8722dc30fd7a11c3982ec648caabc4
Jared Thompson <jtho103@learner.dvusd.org>
2022-03-21 09:29:48 -0700

    your mom

---
+++ ConsoleApp5/ConsoleApp5/weatherforcast.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ConsoleApp5
+{
+    class weatherforcast
+    {
+        public DateTimeOffset Date { get; set; }
+        public int TemperatureCelsius { get; set; }
+        public string Summary { get; set; }
+    }
+}
