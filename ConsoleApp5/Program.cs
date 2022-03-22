cb3004baaf5299e7eee7a8c93f088a40062ea6a3
Jared Thompson <jtho103@learner.dvusd.org>
2022-03-22 09:27:03 -0700

    change number 1

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using static System.Console;
+
+namespace ConsoleApp5
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var weatherForecast = new weatherforcast
+            {
+                Date = DateTime.Parse("2019-08-01"),
+                TemperatureCelsius = 25,
+                Summary = "Hot"
+            };
+            var PhoenixForcast = new weatherforcast
+            {
+                Date = DateTime.Parse("2022-03-22"),
+                TemperatureCelsius = 28,
+                Summary = "Warm"
+            };
+
+            string jsonString = JsonSerializer.Serialize(weatherForecast);
+            WriteLine(jsonString);
+            string phoenixstring = JsonSerializer.Serialize(PhoenixForcast);
+            WriteLine(phoenixstring);
+
+            weatherforcast w = JsonSerializer.Deserialize<weatherforcast>(phoenixstring);
+            WriteLine(w.Summary);
+
+            ReadKey();
+        }
+    }
+
+}
---
+++ ConsoleApp5/weatherforcast.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ConsoleApp5
+{
+    class weatherforcast
+    {
+        public DateTimeOffset Date { get; set; }
+        public int TemperatureCelsius { get; set; }
+        public string Summary { get; set; }
+    }
+}
